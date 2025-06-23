0c5574878dc2e9af3f1b3432ef0c028d1ceb85f2
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-06-23 16:00:42 +0500

    first commit

---
+++ src/data/data.ts
@@ -0,0 +1,131 @@
+export const books1 = [
+	{ name: "📝 Как это видит Билл" },
+	{ name: "🔰 Двенадцать шагов и двенадцать традиций" },
+	{
+		name: "📘 Анонимные Алкоголики",
+	},
+	{
+		name: "🔸 Лучшее из Билла",
+	},
+	{
+		name: "🔻 АА взрослеет",
+	},
+	{
+		name: "🔹 Язык сердца",
+	},
+	{
+		name: "🟡 Доктор Боб и славные старожилы",
+	},
+]
+export const books = [
+	"📝 Как это видит Билл",
+	"🔰 Двенадцать шагов и двенадцать традиций",
+	"📘 Анонимные Алкоголики",
+	"🔸 Лучшее из Билла",
+	"🔻 АА взрослеет",
+	"🔹 Язык сердца",
+	"🟡 Доктор Боб и славные старожилы",
+]
+
+
+export const navigation = [
+	{
+		label: "Анонимные Алкоголики",
+		href: "/aa",
+		icon: "mdi:progress-pencil",
+		style: "",
+	},
+	{
+		label: "Анонимные Наркоманы",
+		href: "/an",
+		icon: "mdi:progress-pencil",
+		style: "",
+	},
+
+	// {
+	// 	label: "",
+	// 	href: "#",
+	// 	target: "",
+	// 	icon: "",
+	// 	style: "",
+	// },
+]
+
+export const navigation_footer = [
+	{
+		category: "Ежедневники",
+		colunm1: [
+			{
+				label: "Анонимные Алкоголики",
+				href: "/aa",
+				icon: "mdi:progress-pencil",
+				style: "underline underline-offset-4 text-blue-500",
+			},
+			{
+				label: "Анонимные Наркоманы",
+				href: "/an",
+				icon: "mdi:progress-pencil",
+				style: "underline underline-offset-4 text-blue-500",
+			},
+			// {
+			// 	label: "Ссылка1",
+			// 	href: "#",
+			// 	target: "_blank",
+			// 	icon: "mdi:progress-pencil",
+			// 	style: "",
+			// },
+		],
+	},
+	// {
+	// 	category: "Категория 2",
+	// 	colunm2: [
+	// 		{
+	// 			label: "Ссылка1",
+	// 			href: "#",
+	// 			target: "_blank",
+	// 			icon: "mdi:progress-pencil",
+	// 			style: "",
+	// 		},
+	// 		{
+	// 			label: "Ссылка1",
+	// 			href: "#",
+	// 			target: "_blank",
+	// 			icon: "mdi:progress-pencil",
+	// 			style: "underline-offset-4 text-blue-500 underline",
+	// 		},
+	// 		// {
+	// 		// 	label: "Ссылка1",
+	// 		// 	href: "#",
+	// 		// 	target: "_blank",
+	// 		// 	icon: "mdi:progress-pencil",
+	// 		// 	style: "",
+	// 		// },
+	// 	],
+	// },
+	// {
+	// 	category: "Категория 3",
+	// 	colunm3: [
+	// 		{
+	// 			label: "Ссылка1",
+	// 			href: "#",
+	// 			target: "_blank",
+	// 			icon: "mdi:progress-pencil",
+	// 			style: "",
+	// 		},
+	// 		{
+	// 			label: "Ссылка1",
+	// 			href: "#",
+	// 			target: "_blank",
+	// 			icon: "mdi:progress-pencil",
+	// 			style: "",
+	// 		},
+	// 		// {
+	// 		// 	label: "Ссылка1",
+	// 		// 	href: "#",
+	// 		// 	target: "_blank",
+	// 		// 	icon: "mdi:progress-pencil",
+	// 		// 	style: "",
+	// 		// },
+	// 	],
+	// },
+]
