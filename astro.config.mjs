466139ee2c5d64d1571f8a202a6eef8689af908b
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-06-24 19:01:07 +0500

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
 		"/alanon": "/alanon/now",
 		"/aa24": "/aa24/now",
 		"/an": "/an/now",
-		"/alanonmm": "/alanonmm /now",
+		"/alanonmm": "/alanonmm/now",
 	},
 	prefetch: {
 		defaultStrategy: "viewport",
